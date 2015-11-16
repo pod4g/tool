254e0b42df174fc60adbd71b62a2b7f708b44efe
LYF <liyanfengbar@hotmail.com>
2015-11-16 21:12:44 +0800

    Create tool.js

---
+++ tool.js
@@ -0,0 +1,115 @@
+/*
+  判断传入的参数的类型
+*/
+function type(arg){
+    var t = typeof arg,s;
+    if(t === 'object'){
+        if(arg === null){
+            return 'null';
+        }else{
+            s = Object.prototype.toString.call(arg);
+            return s.slice(8,s.length-1).toLowerCase();
+        }
+    }else{
+        return t;
+    }
+}
+
+/*
+  判断传入的参数是否是function类型
+*/
+function isFunction(fn){
+    return type(fn) === 'function';
+}
+
+/*
+  判断一个对象是否是 {}
+*/
+function isNullObject(obj){
+   if(obj == null){
+    return true;
+   }
+   for(var attr in obj){
+    return false;
+   }
+   return true;
+}
+
+function getCookie(key){
+    var arr,reg = new RegExp("(^| )"+key+"=([^;]*)(;|$)");
+    if(arr=document.cookie.match(reg)){
+        return decodeURIComponent(arr[2]);
+    }else{
+        return null;
+    }
+}
+
+function deleteCookie(key){
+    var expire = new Date();
+    expire.setTime(expire.getTime() - 1);
+    var cval= getCookie(key);
+    if(cval!=null)
+    document.cookie= key + "="+cval+";expires="+exp.toGMTString();
+}
+
+function deleteCookie(key){
+    var expire = new Date();
+    expire.setTime(expire.getTime() - 1);
+    var cval= getCookie(key);
+    if(cval!=null)
+    document.cookie= key + "="+cval+";expires="+exp.toGMTString();
+}
+
+/*
+  给数组添加遍历方法
+*/
+if(typeof Array.prototype.forEach !== "function"){
+    Array.prototype.forEach = function(fn){
+      var i = 0,l = this.length;
+      while(i<l){
+          fn(this[i],i++);
+      }
+    }
+}
+
+/* 
+  数组归并方法
+*/
+if (typeof Array.prototype.reduce !== "function") {
+  Array.prototype.reduce = function (callback, initialValue ) {
+     var previous = initialValue, k = 0, length = this.length;
+     if (typeof initialValue === "undefined") {
+        previous = this[0];
+        k = 1;
+     }
+    if (typeof callback === "function") {
+      for (k; k < length; k++) {
+         this.hasOwnProperty(k) && (previous = callback(previous, this[k], k, this));
+      }
+    }
+    return previous;
+  }
+
+/*
+ 数组indexOf方法
+*/
+if(typeof Array.prototype.indexOf !== "function"){
+  Array.prototype.indexOf = function(item){
+    for(var i = 0,l = this.length;i < l && item !== this[i];i++);
+    return i === l?-1:i;
+  }
+}
+
+/*
+  判断地版本IE浏览器版本
+  可以判断IE6 7 8 9
+  不能判断IE10 11
+*/
+function isIE(ver){
+  var b = document.createElement("b");
+  b.innerHTML = "<!--[if IE "+ver+"]><i></i><![end if]-->"
+  return b.getElementsByTagName('i').length === 1;
+}
+
+
+
