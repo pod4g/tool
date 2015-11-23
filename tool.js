e20be79b9df798272d5007ec1d53c8d8b9a07d72
LYF <liyanfengbar@hotmail.com>
2015-11-23 15:20:33 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -105,7 +105,9 @@ function deleteCookie(key){
     expire.setTime(expire.getTime() - 1);
     var cval= getCookie(key);
     if(cval!=null)
-    document.cookie= key + "="+cval+";expires="+exp.toGMTString();
+    // 把toGMTString改成了toUTCString，两者等价。但是ECMAScript推荐使用toUTCString方法。toGMTString的存在仅仅是
+    // 为了向下兼容
+    document.cookie= key + "="+cval+";expires="+exp.toUTCString();
 }
 
 
