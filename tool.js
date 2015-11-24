7c77e6f83a5353435a7af922ab1d80b0c6b6520a
LYF <liyanfengbar@hotmail.com>
2015-11-24 15:03:11 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -8,7 +8,8 @@ function type(arg){
             return 'null';
         }else{
             s = Object.prototype.toString.call(arg);
-            return s.slice(8,s.length-1).toLowerCase();
+            //return s.slice(8,s.length-1).toLowerCase();
+            return s.slice(8,-1).toLowerCase();
         }
     }else{
         return t;
