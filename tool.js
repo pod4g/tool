04757ec896721679b0c27f5ca1cfac1e3783f9a2
LYF <liyanfengbar@hotmail.com>
2016-03-30 12:00:25 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -1,4 +1,25 @@
 
+/*
+ 
+   js中浮点数精确计算
+   例如 0.1 + 0.2 = 0.30000000000000004就是有误差所致
+
+*/
+
+function add(num1, num2){
+ var r1, r2, m;
+ r1 = (''+num1).split('.')[1].length;
+ r2 = (''+num2).split('.')[1].length;
+ 
+ m = Math.pow(10,Math.max(r1,r2));
+ return (num1 * m + num2 * m) / m;
+}
+
+console.log(add(0.1,0.2)); //0.3
+console.log(add(0.15,0.2256)); //0.3756
+  
+  
+  
   function getTag(a) {
     var ret = {
         type: a.nodeName
