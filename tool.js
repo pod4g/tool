248d9851297ca70a2054c85e0ed59d1786e08f12
LYF <1601942611@qq.com>
2018-05-18 20:46:05 +0800

    Update tool.js

--- tool.js
+++ tool.js
@@ -1,3 +1,47 @@
+
+const u = navigator.userAgent
+export default {
+  version: {
+    trident: u.indexOf('Trident') > -1, // IE内核
+    presto: u.indexOf('Presto') > -1, // opera内核
+    webKit: u.indexOf('AppleWebKit') > -1, // 苹果、谷歌内核
+    gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') === -1, // 火狐内核
+    mobile: !!u.match(/AppleWebKit.*Mobile.*/), // 是否为移动终端
+    ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), // ios终端
+    android: u.indexOf('Android') > -1 || u.indexOf('Adr') > -1, // android终端
+    iPhone: u.indexOf('iPhone') > -1, // 是否为iPhone或者QQHD浏览器
+    iPad: u.indexOf('iPad') > -1, // 是否iPad
+    webApp: u.indexOf('Safari') === -1, // 是否web应该程序，没有头部与底部
+    weixin: u.indexOf('MicroMessenger') > -1, // 是否微信 （2015-01-22新增）
+    qq: u.match(/\sQQ/i) === ' qq', // 是否QQ
+    // zhushou: (function () {
+    //   const reg = /zhushou ?(\d+(\.\d+){0,2})/
+    //   const zhushou = u.match(reg)
+    //   let version = null
+    //   if (zhushou) {
+    //     version = zhushou[1]
+    //   }
+    //   return version
+    // })(),
+    iPhoneX: (function () {
+      const iOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)
+      const ratio = window.devicePixelRatio || 1
+      const screen = {
+        width: window.screen.width * ratio,
+        height: window.screen.height * ratio
+      }
+      let result = false
+      if (iOS && screen.width === 1125 && screen.height === 2436) {
+        result = true
+      }
+      return result
+    })()
+  },
+  language: (navigator.browserLanguage || navigator.language).toLowerCase(),
+  userAgent: u
+}
+
+
 /*
   实现[ba, bb]之间的随机数
 */
